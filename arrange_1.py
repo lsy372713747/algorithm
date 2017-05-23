1f76d6f9acc9eb4866a641fd96982300075423b8
lsy372713747 <372713747@qq.com>
2017-05-23 20:25:25 +0800

    有1、2、3、4个数字，能组成多少个互不相同且无重复数字的三位数

---
+++ arrange_1.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+'''
+题目：
+有1、2、3、4个数字，能组成多少个互不相同且无重复数字的三位数？都是多少？
+1.程序分析：可填在百位、十位、个位的数字都是1、2、3、4。组成所有的排列后再去
+掉不满足条件的排列。
+'''
+def arrange(loose):
+	i = 0
+	for b1 in loose:
+		for b2 in loose:
+			for b3 in loose:
+				if b1!=b2 and b2!=b3 and b1!=b3:
+					i += 1
+					print "%d. %d%d%d" % (i, b1, b2, b3)
+arrange([1,2,3,4])
